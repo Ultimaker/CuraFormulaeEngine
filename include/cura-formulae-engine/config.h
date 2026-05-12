0776300d67c2ba885ba8399f0f7cf3ae199f5746
Erwan MATHIEU <erwan.mathieu@ultimaker.com>
2026-05-12 08:50:52 +0200

    Use CMake-generated variable
    CURA-13049

--- include/cura-formulae-engine/config.h
+++ include/cura-formulae-engine/config.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #ifdef _WIN32
-#    ifdef CURA_FORMULAE_ENGINE_EXPORT
+#    ifdef cura_formulae_engine_EXPORTS
 #        define CURA_FORMULAE_ENGINE_API __declspec(dllexport)
 #    else
 #        define CURA_FORMULAE_ENGINE_API __declspec(dllimport)
